88e86c78954c1bf45440a39a3e22feedd261c44e
Daniel Leiton <dleitonrivera@gmail.com>
2018-10-17 01:02:47 -0600

    Added .eslintrc.json file for code linting

--- buildScripts/shareMessage.js
+++ buildScripts/shareMessage.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('This app is being shared at: https://bretico.localtunnel.me/'));
+console.log(chalk.green('This app is being shared at: https://bretico.localtunnel.me/')); // eslint-disable-line no-console
+
