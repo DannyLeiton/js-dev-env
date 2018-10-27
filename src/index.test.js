d4048c12709c547c389704f6e37451fcce51c70d
Daniel Leitón <dleiton@informaticos.com>
2018-10-27 15:04:42 -0600

    Feature/add testing and ci (#6)
    * Added test setup
    
    * Added a watcher to the tests so that they run everytime a file is saved
    
    * Added .travis.yml config file and set it up in github

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+	it('should pass', () => {
+		expect(true).to.equal(true);
+	});
+});
+
+describe('index.html', () => {
+	it('should say hello', (done /* Needed for asynchronous tests */) => {
+		const index = fs.readFileSync('./src/index.html', "utf-8");
+		jsdom.env(index, function(err, window) { // It is asynchronous because it has a callback here.
+			const h1 = window.document.getElementsByTagName('h1')[0];
+			// console.log(h1.innerHTML);
+			expect(h1.innerHTML).to.equal('Bienvenido a Bretico!');//equal('Hello World!');
+			done(); // Needed for asynchronous tests, so mocha will know it is ready to be evaluated.
+			window.close();
+		});
+	});
+});
