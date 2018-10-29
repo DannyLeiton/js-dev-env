5fdf8eca9b9e7924366066d9f652b4001a77a9ea
Daniel Leiton <dleitonrivera@gmail.com>
2018-10-29 13:22:01 -0600

    Added /users endpoint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+	// Hard coding for simplicity. Pretend this hits a real database
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+		{"id": 2, "firstName": "Danny", "lastname": "Leiton", "email": "dleitonrivera@gmail.com"},
+		{"id": 3, "firsName": "Pablo", "lastName":"Leiton", "email": "pableiton@hotmail.com"}
+	]);
+});
+
 app.listen(port, function(err) {
 	if(err) {
 		console.log(err);
