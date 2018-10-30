4ebd443119e8e20e698489812f99a4641b0e3c24
Daniel Leiton <dleitonrivera@gmail.com>
2018-10-29 20:40:54 -0600

    Added script to serve mock data via JSON Server

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,11 +1,14 @@
 import 'whatwg-fetch'; // What working group
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
 	return get('users');
 }
 
-function get(url) {
-	return fetch(url).then(onSuccess, onError);
+function get(uri) {
+	return fetch(baseUrl + uri).then(onSuccess, onError);
 }
 
 function onSuccess(res) {
