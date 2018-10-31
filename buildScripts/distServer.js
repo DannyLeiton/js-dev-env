c195e89db9b8c3449d98e66ce5cf6ea53a7727e5
Daniel Leiton <dleitonrivera@gmail.com>
2018-10-31 13:08:18 -0600

    Added TrackJS

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -8,26 +8,17 @@ import compression from 'compression';
 const port = 3000;
 const app = express();
 
-app.use(express.static('dist'));
 app.use(compression());
+app.use(express.static('dist'));
 
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
-app.get('/users', (req, res) => {
-	// Hard coding for simplicity. Pretend this hits a real database
-	res.json([
-		{"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
-		{"id": 2, "firstName": "Danny", "lastName": "Leiton", "email": "dleitonrivera@gmail.com"},
-		{"id": 3, "firstName": "Pablo", "lastName":"Leiton", "email": "pableiton@hotmail.com"}
-	]);
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, function(err) {
-	if(err) {
-		console.log(err);
-	} else {
-		open(`http://localhost:${port}`);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,17 +1,13 @@
-import webpack from 'webpack';
 import path from 'path';
+import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
-//debug: true,
-//noInfo: false,
 
 export default {
-  mode: 'production',
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.json']
-  },
+  debug: true,
   devtool: 'source-map',
+  noInfo: false,
   entry: {
     vendor: path.resolve(__dirname, 'src/vendor'),
     main: path.resolve(__dirname, 'src/index')
@@ -22,39 +18,18 @@ export default {
     publicPath: '/',
     filename: '[name].[chunkhash].js'
   },
-  // Webpack 4 removed the commonsChunkPlugin. Use optimization.splitChunks instead.
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all'
-        }
-      }
-    }
-  },
   plugins: [
-    // Global loader configuration
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-      noInfo: true // set to false to see a list of every file being bundled.
-    }),
-
-    // (separate css and js)
     // Generate an external css file with a hash in the filename
-    //new ExtractTextPlugin('[name].[contenthash].css'),
-    new ExtractTextPlugin('[name].[md5:contenthash:hex:20].css'),
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
 
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
-    // No longer used for Webpack 4. See optimization.splitChunks above instead.
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor'
-    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
 
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
@@ -74,23 +49,23 @@ export default {
       inject: true,
       // Properties you define here are available in index.html
       // using htmlWebpackPlugin.options.varName
-      trackJSToken: 'INSERT YOUR TOKEN HERE'
-    })
+      trackJSToken: '5dbf0059050648a898643993bc3900ea'
+    }),
 
     // Eliminate duplicate packages when generating bundle
-    //new webpack.optimize.DedupePlugin()
+    new webpack.optimize.DedupePlugin(),
 
-    // Code is automatically minified in prod mode as of Webpack 4, so removing this.
     // Minify JS
-    //new webpack.optimize.UglifyJsPlugin()
+	new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: false
+		}
+	})
   ],
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('css-loader?sourceMap')
-      } // extra param is a hint to webpack
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 };
